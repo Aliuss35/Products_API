8e849c54c1a8a388cab337ae490019adffc7f8c1
ali <aliyavuz2000@hotmail.com>
2022-07-15 09:33:49 -0400

    Product properties was fixed

--- products_project/products/models.py
+++ products_project/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-  title =models.CharField(max_length = 255)
+  title =models.CharField(max_length = 150)
   description = models.CharField(max_length = 500)
   price = models.DecimalField(max_digits=8, decimal_places=2)
   inventory_quantity = models.IntegerField()
