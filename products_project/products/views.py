b4d2f1a1830d13dc64759cf8510a21ef12f722d7
ali <aliyavuz2000@hotmail.com>
2022-07-15 09:29:37 -0400

    post update delete added

--- products_project/products/views.py
+++ products_project/products/views.py
@@ -4,16 +4,15 @@ from .serializers import ProductSerializer
 from .models import Product
 from rest_framework.decorators import api_view
 from rest_framework import status
+
 # Create your views here.
 @api_view(['GET', 'POST'])
 def products_list(request):
- 
   if request.method == 'GET':
-    
     products = Product.objects.all()
     serializer = ProductSerializer(products, many = True)
     return Response(serializer.data)
-    
+
   elif request.method == 'POST':
     serializer = ProductSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
